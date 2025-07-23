95d39c025f3932ef0813832836983fbc35e006fe
Hady <hady_awayda@hotmail.com>
2025-07-23 16:53:31 +0300

    Added player collision radius and its respective logic

--- src/movements.c
+++ src/movements.c
@@ -31,18 +31,54 @@ int	on_mouse_move(int x, int y, t_cub *c)
 	return (0);
 }
 
-// int	on_mouse_move(int x, int y, t_cub *c)
-// {
-// 	double	delta;
+/*
+ * is_wall_at: returns true if map cell (mx, my) is a wall or out of bounds.
+ */
+static bool	is_wall_at(int mx, int my, t_map *map)
+{
+	if (my < 0)
+		return (true);
+	if (my >= map->h)
+		return (true);
+	if (mx < 0)
+		return (true);
+	if (mx >= map->w)
+		return (true);
+	if (map->grid[my][mx] == '1')
+		return (true);
+	return (false);
+}
 
-// 	(void)y;
-// 	if (c->mouse_prev_x == -1)
-// 		c->mouse_prev_x = x;
-// 	if (x != c->mouse_prev_x)
-// 	{
-// 		delta = (x - c->mouse_prev_x) * MOUSE_SENS;
-// 		rotate(&c->pl, delta);
-// 		c->mouse_prev_x = x;
-// 	}
-// 	return (0);
-// }
+/*
+ * hit_wall: given world-space (x, y), check the four corners of the
+ * player’s bounding box of radius COLLISION_RADIUS. If any corner
+ * lies in a wall cell, return true.
+ */
+bool	hit_wall(double x, double y, t_map *map)
+{
+	double	corners[4][2];
+	double	r;
+	int		index;
+	int		mx;
+	int		my;
+
+	r = COLLISION_RADIUS;
+	corners[0][0] = x + r;
+	corners[0][1] = y + r;
+	corners[1][0] = x + r;
+	corners[1][1] = y - r;
+	corners[2][0] = x - r;
+	corners[2][1] = y + r;
+	corners[3][0] = x - r;
+	corners[3][1] = y - r;
+	index = 0;
+	while (index < 4)
+	{
+		mx = (int)corners[index][0];
+		my = (int)corners[index][1];
+		if (is_wall_at(mx, my, map) == true)
+			return (true);
+		index++;
+	}
+	return (false);
+}
